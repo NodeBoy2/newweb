7bceffba7e50324db75ecc43268d91abba6ee669
fyf <fengyf_d@163.com>
2018-07-24 22:42:00 +0800

    hello dog

--- webserver/main.go
+++ webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1>Hello, this is my first page!</h1>")
+	io.WriteString(w, "<h1>Hello Dog!</h1>")
 }
 
 func main() {
